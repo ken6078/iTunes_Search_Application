3acfaf63aa300ec5f167a551126bf9d9ddfd5189
ken6078 <ken60786213@gmail.com>
2023-02-03 10:27:46 +0800

    Initial Commit

---
+++ iTunesSearch/iTunesSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesSearchApp.swift
+//  iTunesSearch
+//
+//  Created by Jacky Ben on 2023/2/3.
+//
+
+import SwiftUI
+
+@main
+struct iTunesSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
